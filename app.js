855ea1eef4d15281ee02def71b69d4b3384c6b47
QusaiTA <>
2021-05-06 07:59:39 +0000

    Fixing Error

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var pictureType = prompt('what type of pirctures do u like?');
+
+
+alert('Welcome dear\'s');
+
+
+var pictureType = prompt('what type of pirctures do u like,  Wild Animals, Toursim Area, Football Staduim, or Rain Forset ');
 if(pictureType == 'Wild Animals'){
 
   document.write('<img src="https://images.theconversation.com/files/134652/original/image-20160818-12312-4dyz0u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip">');
@@ -10,11 +15,13 @@ if(pictureType == 'Wild Animals'){
   document.write('<img src="football.png">');
   
 
-}else if(pictureType == "Rain Forest"){
-  document.write('<img src="rainForest">');
+}else if(pictureType == "Rain Forset"){
+  document.write('<img src="rainForest.jpg" width="250px">');
 
-}else 
+}else {
 document.write("<p> u need to identify one of the 4 type's i mention before please " );
+}
+
 
 var name = prompt('hey, i want to know your name ');
-document.write('my name is'+name);
+alert('My name is : ' + name);
